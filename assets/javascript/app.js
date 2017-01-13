25e2a462b46437621e5616fd8ebdd196a5bb5979
REvanCof <rogerecoffey@gmail.com>
2017-01-13 16:39:32 -0600

    Update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,11 +1,8 @@
-var complementBank = ["Cool shoes","Love your outfit","Your hair looks great today","You smell wonderful","You are an inspiration","You look like a winner","You're just the best","You're so clever","Seriously, you're awesome", "You are an impressive person", "Keep up the good work", "You should be proud of yourself", "Tone it down some, you're making everyone else look bad"];
+//Feel free to add more complements
+var complementBank = ["Cool shoes","I love your outfit","Your hair looks great today","You smell wonderful","You are an inspiration","You look like a winner","You're just the best","You're so clever","Seriously, you're awesome","You are an impressive person","Keep up the good work, champ","You should be proud of yourself","Tone it down some, you're making everyone else look bad"];
 
+//Returns a random complement from complementBank[]
 function randomComplement() {
-	var randomNum = Math.floor(Math.random() * 13) + 1;
+	var randomNum = Math.floor(Math.random() * complementBank.length);
 	return complementBank[randomNum];
 }
-
-$( document ).ready(function() {
-	var complement = randomComplement();
-	$( "#footerFloatRight" ).html(complement);
-})
